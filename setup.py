8531bdd7e2aaca78fd1bf110bf4df5eb55031208
Josh Klar <j@iv597.com>
2016-10-14 17:14:35 -0500

    Oops, forgot packages

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 
-setup(name='fabric', version='1.0.0+alpha0', description='tav/pylibs fork of Fabric')
+setup(name='fabric', version='1.0.0+alpha0', description='tav/pylibs fork of Fabric', packages=[str("fabric")])
