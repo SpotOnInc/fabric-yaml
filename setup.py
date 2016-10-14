8df75c66c345a36641162e8e78bb04d5c325595b
Josh Klar <j@iv597.com>
2016-10-14 16:59:33 -0500

    Should dramatically simplify setup.py

--- setup.py
+++ setup.py
@@ -1,54 +1,5 @@
-#! /usr/bin/env python
+from setuptools import setup
 
-# Public Domain (-) 2010-2011 The Ampify Authors.
-# See the Ampify UNLICENSE file for details.
+from fabric.version import VERSION
 
-import sys
-
-from distutils.core import Extension, setup
-from os.path import dirname, join as join_path, realpath
-
-try:
-    from pyutil.env import run_command
-except ImportError:
-    from subprocess import call
-    def run_command(args, cwd, **kwargs):
-        retcode = call(args, cwd=cwd)
-        if retcode and kwargs.pop('exit_on_error', None):
-            sys.exit(1)
-
-# ------------------------------------------------------------------------------
-# the extensions
-# ------------------------------------------------------------------------------
-
-extensions = [
-    Extension("simplejson._speedups", ["simplejson/_speedups.c"]),
-    ]
-
-# ------------------------------------------------------------------------------
-# run setup
-# ------------------------------------------------------------------------------
-
-if not sys.argv[1:]:
-    sys.argv.extend(['build_ext', '-i'])
-
-setup(
-    name="pylibs",
-    version="git",
-    description="A collection of Python libraries",
-    ext_modules=extensions,
-    )
-
-pylibs_path = dirname(realpath(__file__))
-packages_path = ['pycrypto']
-
-if sys.version_info < (2, 6):
-    packages_path.append('pyssl')
-
-for path in packages_path:
-    path = join_path(pylibs_path, path)
-    run_command(
-        [sys.executable, join_path(path, 'setup.py')] + sys.argv[1:],
-        exit_on_error=True, cwd=path, redirect_stdout=False,
-        redirect_stderr=False
-        )
+setup(name='fabric', version=VERSION, description='tav/pylibs fork of Fabric')
