7d5a153ea0b3d9a15161361053578a9ca2e52228
Josh Klar <j@iv597.com>
2016-10-17 16:47:57 -0500

    Updated packages list

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
 from setuptools import setup
 
-setup(name='fabric', version='1.0.0+alpha0', description='tav/pylibs fork of Fabric', packages=[str("fabric")])
+setup(
+    name='fabric',
+    version='1.0.0+alpha0',
+    description='tav/pylibs fork of Fabric',
+    packages=[
+        str("fabric"),
+        str("fabric.contrib"),
+    ]
+)
